186fedd124fc423b37490df2ac2afc3308ddcd08
TobeCA12381 <168382947+TobeCA12381@users.noreply.github.com>
2024-06-15 01:19:38 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 const http = require('node:http');
 
-const hostname = '104.198.215.40';
+const hostname = '0.0.0.0';
 const port = 8093;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
-});
+  res.setHeader('Content-Type', 'text/json');
+  let arreglo = [{
+    nombre:"Juan",
+    apellido:"Perez"
+    },
+    {
+    nombre:"Betox",
+    apellido:"Alarcon"
+    },
+    {
+    nombre:"Anthony",
+    apellido:"Garcia"
+    }
+    ]
+    
+    res.write(JSON.stringify(arreglo));  
+    res.end('');
+    
+  });
+
+
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
-});
+}); 
