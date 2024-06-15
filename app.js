c0966fbece91207b487b6a6f8fb45b308f39f156
TobeCA12381 <168382947+TobeCA12381@users.noreply.github.com>
2024-06-15 01:23:20 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const app = express()
-
+const express = require("express"); // Importar el módulo express
 const path = require('path');
 
-app.set("view engine","ejs")
+const app = express();
+
+app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, 'public')));
-app.get("/",(req,res)=>{
 
-    res.render("index")
-})
+app.get("/", (req, res) => {
+    res.render("index");
+});
 
-app.listen(8093,'0.0.0.0',(req,res)=> {
-    console.log("Corriendo  en el puerto 0893")
-
-})
+app.listen(8093, '0.0.0.0', () => {
+    console.log("Corriendo en el puerto 8093");
+});
