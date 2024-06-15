e659eacf6e02d3d64f392c9ea5bba5f7671eafa2
TobeCA12381 <168382947+TobeCA12381@users.noreply.github.com>
2024-06-15 00:37:13 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,68 +1,17 @@
-const http = require('node:http');
-const fs = require('fs');
+const express = require("express");
 const path = require('path');
-const ejs = require('ejs');
 
+const app = express();
 const hostname = '34.134.26.143';
 const port = 8093;
 
-// Ruta al directorio de archivos estáticos y vistas
-const publicDir = path.join(__dirname, 'public');
-const viewsDir = path.join(__dirname, 'views');
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, 'public')));
 
-const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    // Renderizar la vista EJS
-    const filePath = path.join(viewsDir, 'index.ejs');
-    ejs.renderFile(filePath, {}, (err, str) => {
-      if (err) {
-        res.statusCode = 500;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('Error al renderizar la vista EJS');
-      } else {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html');
-        res.end(str);
-      }
-    });
-  } else {
-    // Servir archivos estáticos
-    const staticFilePath = path.join(publicDir, req.url);
-    fs.readFile(staticFilePath, (err, data) => {
-      if (err) {
-        res.statusCode = 404;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('Archivo no encontrado');
-      } else {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', getContentType(req.url));
-        res.end(data);
-      }
-    });
-  }
+app.get("/", (req, res) => {
+    res.render("index");
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, hostname, () => {
+    console.log(`Servidor corriendo en http://${hostname}:${port}/`);
 });
-
-// Función para obtener el tipo de contenido según la extensión del archivo
-function getContentType(url) {
-  const ext = path.extname(url);
-  switch (ext) {
-    case '.html':
-      return 'text/html';
-    case '.css':
-      return 'text/css';
-    case '.js':
-      return 'application/javascript';
-    case '.png':
-      return 'image/png';
-    case '.jpg':
-      return 'image/jpeg';
-    case '.gif':
-      return 'image/gif';
-    default:
-      return 'application/octet-stream';
-  }
-}
